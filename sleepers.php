448416c0686dc52f267084a72d0b67f7f50c817e
HappyDev95 <Sam_hapgood@yahoo.com>
2017-12-06 16:18:29 -0500

    displays sleepers, samHap

---
+++ sleepers.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Sleepers</title>
+	
+	<style type="text/css"> </style>
+	
+	 <script type ="text/javascript">
+        function goToNewPage()
+        {
+            var url = document.getElementById('list').value;
+            if(url != 'none') {
+                window.location = url;
+            }
+        }
+    </script>
+</head>
+<body>
+
+<?php
+	header('Loacation: http://www.nfl.com/news');
+?>
+</body>
+</html>
